022b5a6a75968fe68f3838835ee21872a5fb887c
Josh Malmquist <joshmalmquist96@gmail.com>
2018-05-18 15:54:00 -0500

    Initialize base Quartermaster project + Updated Readme
    Initialized Node.js project and added Discord.js. Package.json has the details.
    
    Got basic functionality working in discord channel.
    
    current Commands:
    !server = will provide server name and number of total members
    !ping = will respond back with pong.
    
    Next Step: will be to test hosting bot on heroku.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Quartermaster
 Quartermaster is a discord bot used to create game events for server members and preform basic moderation/admin/server quality of life tasks.
+
+## Libraries and Dependencies
+
+*Node.js
+*Discord.js
+*async-limiter
+*long
+*prisim-media
+*safe-buffer
+*snekfetch
+*tweetnacl
+*ws
+
+
+## How to Build
+To run and build Quartermaster simply install node.js and install discord.js using `npm install discord.js`.
+
+
+## Current Commands
+| Command | Description |
+|---------|-------------|
+|`!server`| Will provide the server name and number of members |
+|`!ping`  | Quartermaster will respond with Pong |
---
+++ quartermaster.js
@@ -0,0 +1,30 @@
+// require the discord.js module
+const Discord = require('discord.js');
+const config = require('./config.json');
+const token = require('./token.json'); //Hold the bot token
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will trigger whenever your bot:
+// - finishes logging in
+// - reconnects after disconnecting
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+	console.log(message.content);
+	if(message.content.startsWith(`${config.prefix}ping`)){
+		//Send back "Pong" to the channel the message was sent in
+		message.channel.send('Pong');
+		console.log('pong');
+	}
+	else if(message.content === `${config.prefix}server`){
+		message.channel.send(`This server's name is: ${message.guild.name} \nTotal members: ${message.guild.memberCount}`);
+	}
+});
+
+// login to Discord with your app's token
+client.login(token.token);
